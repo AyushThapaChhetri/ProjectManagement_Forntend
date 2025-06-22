88d49e26520ded1a6121168b9c943247f5e58aae
AyushThapaChhetri <ayushthapa369@gmail.com>
2025-06-23 00:42:19 +0545

    Few changes in boards

--- src/pages/board/Board.tsx
+++ src/pages/board/Board.tsx
@@ -1,9 +1,65 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, HStack, Icon, Text } from "@chakra-ui/react";
+import { TbLetterF } from "react-icons/tb";
+import { LuLayoutTemplate } from "react-icons/lu";
+import BoardCard from "./BoardCard";
 
 const Board = () => {
   return (
     <>
-      <Flex>Hello</Flex>
+      <Flex px={"15%"} py={"50px"} direction={"column"}>
+        <Flex
+          gap={5}
+          pb={"20px"}
+          borderBottom={"2px solid grey"}
+          w={"100%"}
+          pl={"100px"}
+        >
+          <Flex
+            border={"2px solid black"}
+            height={"50px"}
+            width={"50px"}
+            justify={"center"}
+            align={"center"}
+            rounded={"md"}
+          >
+            <Icon size="2xl">
+              <TbLetterF />
+            </Icon>
+          </Flex>
+          <Text textStyle="2xl" textAlign={"justify"}>
+            Focus Workspace
+          </Text>
+        </Flex>
+
+        <Flex py={10} direction={"column"} gap={3}>
+          <Text textStyle="2xl" textAlign={"justify"}>
+            Boards
+          </Text>
+          <HStack>
+            <LuLayoutTemplate />
+            <Text textStyle="lg" textAlign={"justify"}>
+              Most popular templates
+            </Text>
+          </HStack>
+          <Text textStyle="lg" textAlign={"justify"}>
+            To help you get started faster, we offer a variety of ready-to-use
+            templates designed for different needs. Whether you're managing a
+            software project, planning personal goals, organizing team tasks, or
+            tracking a content calendar our most popular templates give you a
+            solid foundation. Simply choose a template, customize it to fit your
+            workflow, and you're ready to go. Save time and stay focused with
+            layouts that work out of the box.
+          </Text>
+          <Flex justify={"space-between"} gap={10} wrap={"wrap"} mt={"50px"}>
+            <BoardCard Boardheading={"Basic Board"} backgroundColor={"blue"} />
+            <BoardCard Boardheading={"Kanban Board"} backgroundColor={"pink"} />
+            <BoardCard
+              Boardheading={"Daily Task Management"}
+              backgroundColor={"orange"}
+            />
+          </Flex>
+        </Flex>
+      </Flex>
     </>
   );
 };
---
+++ src/pages/board/BoardCard.tsx
@@ -0,0 +1,28 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+interface BoardCardProps {
+  Boardheading: string;
+  backgroundColor: string;
+}
+const BoardCard = ({ Boardheading, backgroundColor }: BoardCardProps) => {
+  return (
+    <>
+      <Flex
+        w={{ base: "100%", desktop: "350px" }}
+        // border={"1px solid gray"}
+        bg={backgroundColor}
+        color={"white"}
+        fontWeight={"bold"}
+        p={"2rem"}
+        shadow={"md"}
+        rounded={"md"}
+      >
+        <Text textStyle="lg" textAlign={"justify"}>
+          {Boardheading}
+        </Text>
+      </Flex>
+    </>
+  );
+};
+
+export default BoardCard;
